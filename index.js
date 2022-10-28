980d928302d43042f7f2736f8639af9f90411f3c
Raad <ashabulraad@gmail.com>
2022-10-28 17:20:46 +0600

    Filter category

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.get('/', (req, res) => {
     res.send('Server is active.');
 });
 
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '00')
+        res.send(courses);
+    else {
+        const category = categories.filter(course => course.category_id === id);
+        res.send(category);
+    }
+});
+
 app.listen(port, () => {
     console.log('Server is active on port: ', port);
 });
