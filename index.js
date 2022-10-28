cd62f180cedc3e38bd12160182d308859edd4e97
Raad <ashabulraad@gmail.com>
2022-10-28 17:24:20 +0600

    Find course

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ app.get('/category/:id', (req, res) => {
     }
 });
 
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
+});
+
 app.listen(port, () => {
     console.log('Server is active on port: ', port);
 });
