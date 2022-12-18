03b0e84e1705ad0ed3c58b88d1eb955e8289d0f5
Yamin Raad <ashabulraad@gmail.com>
2022-12-18 18:47:06 +0600

    Update

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 const categories = require('./Data/categories.json');
-// const courses = require('./Data/courses.json');
 
 const uri = "mongodb+srv://dbUser01:wMnkxV8aWZGDtgyy@cluster0.dq2ahkq.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -55,34 +54,6 @@ const run = async () => {
 }
 
 run().catch(error => console.log(error));
-
-// app.get('/', (req, res) => {
-//     res.send('Server is active.');
-// });
-
-// app.get('/course-categories', (req, res) => {
-//     res.send(categories);
-// });
-
-// app.get('/category/:id', (req, res) => {
-//     const id = req.params.id;
-//     if (id === '00')
-//         res.send(courses);
-//     else {
-//         const category = courses.filter(course => course.category_id === id);
-//         res.send(category);
-//     }
-// });
-
-// app.get('/courses', (req, res) => {
-//     res.send(courses);
-// });
-
-// app.get('/courses/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedCourse = courses.find(course => course._id === id);
-//     res.send(selectedCourse);
-// });
 
 app.get('/', (req, res) => {
     res.send('Server is active.');
